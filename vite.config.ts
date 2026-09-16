65f8f086354527d5be79593be195f5405ba78ed5
jokerfin286 <107624485+jokerfin286@users.noreply.github.com>
2026-09-16 12:23:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,12 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: 'Polygon-Siege',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
